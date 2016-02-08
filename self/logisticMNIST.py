5c79eb0f66512d11e0c381df808d769b04a1825a
CoderHam <hemanham@gmail.com>
2016-02-08 20:22:11 +0530

    notMNIST simple neural net

--- self/logisticMNIST.py
+++ self/logisticMNIST.py
@@ -1,8 +1,6 @@
-from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 from six.moves import cPickle as pickle
-from six.moves import range
 
 pickle_file = 'notMNIST.pickle'
 
